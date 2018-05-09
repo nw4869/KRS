ce62af0db3458587ae11501b16a69507d1bc2a5a
nw4869 <nw4869@gmail.com>
2018-05-09 15:24:48 +0800

    update account views

--- account/views.py
+++ account/views.py
@@ -1,11 +1,19 @@
+from django import forms
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 
 
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(label='邮箱', max_length=200, help_text='必填。')
+
+    class Meta(UserCreationForm.Meta):
+        fields = ("username", "email")
+
+
 class RegisterView(CreateView):
     template_name = 'registration/register.html'
-    form_class = UserCreationForm
+    form_class = RegisterForm
     success_url = '/'
 
     def form_valid(self, form):
